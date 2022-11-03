6eefd0ba08f7e4075e958ea1e2ff451bb103e873
Simenya J.e <simenyajonathan@gmail.com>
2022-11-03 11:47:43 +0300

    Initial project state, webview no login

---
+++ app/src/main/java/com/application/kyucsa/WebActivity.kt
@@ -0,0 +1,28 @@
+package com.application.kyucsa
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class WebActivity : AppCompatActivity() {
+    private  lateinit var   webView:WebView
+    private   var URL="https://simenya.github.io/kyucsa/"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web)
+        webView=findViewById(R.id.web)
+        webView.apply {
+            webViewClient= WebViewClient()
+            loadUrl(URL)
+            settings.javaScriptEnabled=true
+
+        }
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) webView.goBack() else super.onBackPressed()
+
+    }
+}
